57af5396a46759add9186ec960e4f6528f2b6842
usac201602872 <2994865830101@ingenieria.usac.edu.gt>
2020-06-22 23:20:32 -0600

    Parcial 2.3

--- Cliente.py
+++ Cliente.py
@@ -10,12 +10,27 @@ import datetime
 import binascii
 
 SERVER_IP = '' #Colocar en el medio de las comillas la ip a utilizar
-SERVER_PORT = 9817
-BUFFER_SIZE = 64 * 1024
+SERVER_PORT = 9817 #Puerto del Socket a utilizar
+BUFFER_SIZE = 64 * 1024 #Buffer de 64 KB
+
+# Credenciales de MQTT
 MQTT_HOST = '167.71.243.238'
 MQTT_PORT = '1883'
 MQTT_USER = 'proyectos'
 MQTT_PASS = 'proyectos980'
+
+#Cliente de MQTT
+client = paho.Client(clean_session=True)
+client.username_pw_set(MQTT_USER,MQTT_PASS)
+client.connect(host=MQTT_HOST, port=MQTT_PORT)
+
+Separador = '/'
+Sala = 'S17'
+ID = '202058457'
+Peso = '5741'
+
+topic = Sala + Separador + ID + Separador + Peso
+
 
 while True:
     
@@ -52,7 +67,7 @@ while True:
         # Esperamos la respuesta del ping servidor
         bytesR = 0  #Bit a ser Recividos
         bytesE = len(message) #Bits a ser enviados
-        
+
     finally:
         print('\n\nConexion finalizada con el servidor')
         sock.close()
