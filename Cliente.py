406cda7698bc76ce0b9d04476d3c0bcc6320c533
usac201602872 <2994865830101@ingenieria.usac.edu.gt>
2020-06-24 09:26:12 -0600

    Parcial 2.6.1

--- Cliente.py
+++ Cliente.py
@@ -1,34 +1,33 @@
 import paho.mqtt.client as mqtt #importamos libreria de mqtt paho
 import logging #Loggin Reemplaza al print
-import time
+import time  #
 import os
 import threading 
 import sys  
 import binascii
 import datetime
 
-
-class Desicion:
-    def __init__(self,entrada,grupo,id):
+class Desicion(object):                     # Clase de desicion para obtener mas facil el topic
+    def __init__(self,entrada,grupo,id):    # Los valores del topic en string
         self.entrada = entrada
         self.grupo = grupo
         self.id = id
     
-    def getGrupo(self):
+    def getGrupo(self):         #Obtener el Grupo
         return self.grupo
     
-    def getID(self):
+    def getID(self):            #Obtener la ID (carnet o Sala)
         return self.id
     
-    def getEntrada(self):
+    def getEntrada(self):       #Obtener el input(mensaje o audio)
         return self.entrada
     
-    def modifyDesicion(self,NE,NG,NI):
+    def modifyDesicion(self,NE,NG,NI): #Para poder modificar el topic
         self.entrada = NE
         self.grupo = NG
         self.id = NI
 
-class ComandoInvalido(Exception):
+class ComandoInvalido(Exception): #Excepcion creada especificamente Por si un comando ingresado no es el correcto
     def __init__(self,comando):
         self.comando = comando
     
@@ -42,7 +41,6 @@ class Name(Exception):
     def __str__(self):
         return str("Feliz Dia")
 
-
 # Credenciales para el archivo de audio
 SERVER_IP = '' #Colocar en el medio de las comillas la ip a utilizar
 SERVER_PORT = 9817 #Puerto del Socket a utilizar
@@ -54,7 +52,6 @@ MQTT_PORT =  1883        #numero de Puerto
 MQTT_USER = "proyectos" #Nombre del usuario del MQTT
 MQTT_PASS = "proyectos980"
 
-
 #nombre = input("dime tu nombre: ")#con el input lo que hacemos es solicitar el nombre del participante
                                 #se guarda en la variable nombre
 
@@ -63,15 +60,13 @@ def on_connect(client, userdata, flags, rc):#funcion que se ejecuta por la libre
     client.subscribe(topic) #topic donde se va a suscribir
     client.publish(topic) #enviamos un mensaje con el metodo "publish" con el topico raiz avisando que hemos ingresado
                                                              
-
 def on_message(client, userdata, msg): #declaramos una funcion que sera ejecutada por la libreria MQTT cuando llegue un mensaje desde el servidor
     print(msg.payload.decode("utf-8"))#se imprime el mensaje que haya llegado
-    #msg.payload.decode("audio.wav")#se reproduce el mensaje que haya llegado
-    #logging.info('Grabacion finalizada, inicia reproduccion')#Iniciacion y Visualizacion de la informacion final de la grabacion de audio
-    #os.system('aplay audio.wav')
+    msg.payload.decode("audio.wav")#se reproduce el mensaje que haya llegado
+    os.system('aplay audio.wav')
 
-print("Bienvenido al Chat hecho con MQTT.\nPor favor, ingrese los siguientes datos")
-
+print("Bienvenido al Chat hecho con MQTT."
+    "\nPor favor, ingrese los siguientes datos")
 
 #-------------------------------------------- Configuracion Inicial para conectarse al broker --------------------------------
 
@@ -85,11 +80,10 @@ cliente.loop_start() #si esto no se ejecuta todo el tiempo no podemos recibir me
 
 def audio_thread(tiempo):  # Hilo solo de audio
     logging.info('Comenzando grabacion')
-    os.system('arecord -d '+tiempo+' -f U8 -r 8000 audio.wav')
-    f = open ("audio.wav", "rb")
-    enviar_audio = f.read(BUFFER_SIZE)
+    os.system('arecord -d '+tiempo+' -f U8 -r 8000 audio.wav') # comando bah en la terminal para audio
+    f = open ("audio.wav", "rb")    #Lo abre en bytes
+    enviar_audio = f.read(BUFFER_SIZE) 
     cliente.publish(topic, enviar_audio) #Se publica el audio que se quiere enviar
-
 
 try:
     e = input('Que tipo de mensaje quiere enviar(mensaje, audio):')
@@ -99,33 +93,31 @@ try:
 except KeyboardInterrupt:
     print('\n\nSe ha deconectado del servidor')
 
+# Credenciales para topics
+separador = '/'
+Entrada = Persona.getEntrada()
+Grupo = Persona.getGrupo()
+ID = Persona.getID()
+topic = Entrada+separador+Grupo+separador+ID
+
 
 while True: #generacion de bucle infinito para ir solicitando al usuario que vaya ingresando los mensajes
-        # Credenciales para topics
     try: 
-         
-        separador = '/'
-        Entrada = Persona.getEntrada()
-        Grupo = Persona.getGrupo()
-        ID = Persona.getID()
-
-        topic = Entrada+separador+Grupo+separador+ID
-        
         if Entrada == 'mensaje':
             a_enviar = input ("Escribe un mensaje ---> ")#Aca se solicita el mensaje
             a_enviar = ID + ": " + a_enviar #Se concatena y se incluye el nombre dentro del mensaje
             cliente.publish(topic, a_enviar) #Se publica el mensaje que se quiere enviar
             d = input("Quiere seguir enviando otro tipo de mensaje a un usuario diferente? [Y/N]:") #Para seguir usando el programa y cambiar el topic
-            if d == "Y" or d == "y":
+            if d == "Y" or d == "y": # Para enviar a otro usuario
                 ne = input('Que tipo de mensaje quiere enviar(mensaje, audio):')
                 ng = input('Entre a que grupo quiere ir(solo el numero):')
                 ni = input('Entre a que usuario(carne o sala) quiere enviar:')
-                Persona.modifyDesicion(ne,ng,ni)
+                Persona.modifyDesicion(ne,ng,ni) #modifica el topic
             elif d == "N" or d == "n":
                     print ('Conexion con el servidor finalizada')
                     break
             else:
-                raise ComandoInvalido(d)
+                raise ComandoInvalido(d) #Levanta excepcion si no colocaalguna de las opciones
             
         elif Entrada == 'audio':
             duracion = input("Cuanto tiempo, en s, quiere grabar? :")
@@ -133,16 +125,16 @@ while True: #generacion de bucle infinito para ir solicitando al usuario que vay
             voice.start() # Para iniciar el hilo
             voice.join() # espera a que el hilo termine, sin este, solo con un time.sleep(1) para continuar con el programa.
             d = input("Quiere seguir enviando otro tipo de mensaje a un usuario diferente? [Y/N]:")
-            if d == "Y" or d == "y":
+            if d == "Y" or d == "y": #Para enviar a otro usuario
                 ne = input('Que tipo de mensaje quiere enviar(mensaje, audio):')
                 ng = input('Entre a que grupo quiere ir(solo el numero):')
                 ni = input('Entre a que usuario(carne o sala) quiere enviar:')
-                Persona.modifyDesicion(ne,ng,ni)
-            elif d == "N" or d == "n":
+                Persona.modifyDesicion(ne,ng,ni) #modifica el topic
+            elif d == "N" or d == "n": #Para salir de la aplicacion
                 print ('Conexion con el servidor finalizada')
                 break
             else:
-                raise ComandoInvalido(d)
+                raise ComandoInvalido(d) #Levanta excepcion si no coloca alguna de las dos opciones
         else:
             raise ComandoInvalido(Entrada)
     except KeyboardInterrupt:
